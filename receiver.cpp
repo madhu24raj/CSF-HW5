19d24c98f2211c1fcf91e743b4563f933a22a5bc
madhu24raj <mrajapr2@jh.edu>
2025-11-17 13:31:48 -0500

    Implement connection handling and message exchange in receiver.cpp

--- receiver.cpp
+++ receiver.cpp
@@ -21,13 +21,67 @@ int main(int argc, char **argv) {
   Connection conn;
 
   // TODO: connect to server
-  
+  conn.connect(server_hostname, server_port);
+
+  if (!conn.is_open()) {
+    std::cerr << "Failed to connect to server\n";
+    return 1;
+  }
+
 
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
 
+  Message rlogin(TAG_RLOGIN, username);
+  if (!conn.send(rlogin)) {
+    std::cerr << "Failed to send rlogin message\n";
+    return 1;
+  }
+
+  Message response1;
+  if (!conn.receive(response1)) {
+    std::cerr << "Failed to receive response for rlogin message\n";
+    return 1;
+  }
+
+  if (response1.tag != TAG_OK) {
+    if (response1.tag == TAG_ERR) {
+      std::cerr << response1.data << "\n";
+      return 1;
+    } 
+    else {
+      std::cerr << "Unexpected response for rlogin message" << response1.tag << "\n";
+      return 1;
+    }
+  }
+
+  Message join(TAG_JOIN, room_name);
+  if (!conn.send(join)) {
+    std::cerr << "Failed to send join message\n";
+    return 1;
+  }
+
+  Message response2;
+  if (!conn.receive(response2)) {
+    std::cerr << "Failed to receive response for join message\n";
+    return 1;
+  }
+
+  if (response2.tag != TAG_OK) {
+    if (response2.tag == TAG_ERR) {
+      std::cerr << response2.data << "\n";
+      return 1;
+    } 
+    else {
+      std::cerr << "Unexpected response for join message" << response2.tag << "\n";
+      return 1;
+    }
+  }
+
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
+
+  
 
 
   return 0;
