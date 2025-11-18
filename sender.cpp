f27f8774d7152d42c69c06211061841b36112298
madhu24raj <mrajapr2@jh.edu>
2025-11-17 19:32:31 -0500

    connect to server and slogin msg - sender

--- sender.cpp
+++ sender.cpp
@@ -21,9 +21,43 @@ int main(int argc, char **argv) {
   server_port = std::stoi(argv[2]);
   username = argv[3];
 
+  Connection conn;
+
   // TODO: connect to server
+  conn.connect(server_hostname, server_port);
+
+  if (!conn.is_open()) {
+    std::cerr << "Failed to connect to server\n";
+    return 1;
+  }
+
 
   // TODO: send slogin message
+
+
+  Message slogin(TAG_SLOGIN, username);
+  if (!conn.send(slogin)) {
+    std::cerr << "Failed to send rlogin message\n";
+    return 1;
+  }
+
+  Message response1;
+  if (!conn.receive(response1)) {
+    std::cerr << "Failed to receive response for rlogin message\n";
+    return 1;
+  }
+
+  if (response1.tag != TAG_OK) {
+    if (response1.tag == TAG_ERR) {
+      std::cerr << response1.data << "\n";
+      return 1;
+    } 
+    else {
+      std::cerr << "Unexpected response for rlogin message" << response1.tag << "\n";
+      return 1;
+    }
+  }
+
 
   // TODO: loop reading commands from user, sending messages to
   //       server as appropriate
