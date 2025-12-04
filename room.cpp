a92e62383d494fd5af0812e10ff52d66f3d8004a
Ryan Kwok <kwokryan11@gmail.com>
2025-12-04 13:31:48 -0500

    Finished room implementation

--- room.cpp
+++ room.cpp
@@ -7,20 +7,35 @@
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
   // TODO: initialize the mutex
+  pthread_mutex_init(&lock, nullptr);
 }
 
 Room::~Room() {
   // TODO: destroy the mutex
+  pthread_mutex_destroy(&lock);
 }
 
 void Room::add_member(User *user) {
   // TODO: add User to the room
+  pthread_mutex_lock(&lock);
+  members.insert(user);
+  pthread_mutex_unlock(&lock);
 }
 
 void Room::remove_member(User *user) {
   // TODO: remove User from the room
+  pthread_mutex_lock(&lock);
+  members.erase(user);
+  pthread_mutex_unlock(&lock);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
   // TODO: send a message to every (receiver) User in the room
+  pthread_mutex_lock(&lock);
+  for (User* user: members) {
+    Message* message = new Message(TAG_DELIVERY, message_text);
+    user->mqueue.enqueue(message);
+  }
+  pthread_mutex_unlock(&lock);
+
 }
