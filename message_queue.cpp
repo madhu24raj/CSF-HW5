052e42dddfd35960959b85a31ac6f8a1ebfbc7c8
Ryan Kwok <kwokryan11@gmail.com>
2025-12-04 00:09:18 -0500

    Finished implementation of message_queue

--- message_queue.cpp
+++ message_queue.cpp
@@ -4,17 +4,28 @@
 
 MessageQueue::MessageQueue() {
   // TODO: initialize the mutex and the semaphore
+  // mutex is in unlocked state
+  pthread_mutex_init(&m_lock, NULL);
+  // start the counter at 0 messages
+  sem_init(&m_avail, 0, 0);
 }
 
 MessageQueue::~MessageQueue() {
   // TODO: destroy the mutex and the semaphore
+  pthread_mutex_destroy(&m_lock);
+  sem_destroy(&m_avail);
+
 }
 
 void MessageQueue::enqueue(Message *msg) {
   // TODO: put the specified message on the queue
-
+  pthread_mutex_lock(&m_lock);
+  m_messages.push_back(msg);
+  pthread_mutex_unlock(&m_lock);
   // be sure to notify any thread waiting for a message to be
   // available by calling sem_post
+  sem_post(&m_avail);
+  
 }
 
 Message *MessageQueue::dequeue() {
@@ -31,8 +42,15 @@ Message *MessageQueue::dequeue() {
 
   // TODO: call sem_timedwait to wait up to 1 second for a message
   //       to be available, return nullptr if no message is available
+  int wait_status = sem_timedwait(&m_avail, &ts);
+  if (wait_status == -1) {
+    return nullptr;
+  } 
+  pthread_mutex_lock(&m_lock);
 
   // TODO: remove the next message from the queue, return it
-  Message *msg = nullptr;
+  Message *msg = m_messages.front();
+  m_messages.pop_front();
+  pthread_mutex_unlock(&m_lock);
   return msg;
 }
